b019f1b51de3c196140fe6681696b1da5c08e988
Ilya Valchanka <Ilya.Valchanka@thomsonreuters.com>
2021-04-26 23:35:23 +0300

    Pushed the project skeleton.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aws_sdk_for_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
